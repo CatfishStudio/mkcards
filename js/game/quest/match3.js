dcad5ed593dd751210df6f8ef7ba501f5bd46bad
CatfishStudio <somov@i.ua>
2015-09-19 11:09:26 +0300

    Match3 exchange units - complete

--- js/game/quest/match3.js
+++ js/game/quest/match3.js
@@ -23,8 +23,10 @@ var matchMatrixBackPosition = new Object();		// Матрица позиций x,
 var matchSelectUnit1 = null;		// выбранный первый юнит
 var matchSelectUnit2 = null;		// выбран второй юнит
 
+var matchFieldBlocked = false;		// блокирование игрового поля
+
 /* Инициализация матриц позиций ================================================================ */
-function initMatchMatrixPosotion()
+function initMatchMatrixPosition()
 {
 	matchMatrixFrontPosition = new Object();
 	matchMatrixBackPosition = new Object();
@@ -45,18 +47,18 @@ function initMatchMatrixPosotion()
 /* Создание игрового поля ====================================================================== */
 function createMatchField(levelJSON)
 {
-	initMatchMatrixPosotion();
+	initMatchMatrixPosition();
 
 	matchStage = new PIXI.Container();
 	matchMatrixCell = new Object();
 	matchMatrixUnit = new Object();
 
+	/* ячейки */
 	var index = 0;
-	for(var i = 0; i < MATCH_COLUMNS; i++)
+	for(var iCell = 0; iCell < MATCH_COLUMNS; iCell++)
 	{
-		for(var j = 0; j < MATCH_ROWS; j++)
+		for(var jCell = 0; jCell < MATCH_ROWS; jCell++)
 		{
-			/* ячейка */
 			if(levelJSON.data.Level.cell[index].cellType != MATCH_CELL_TYPE_DROP)
 			{
 				var graphics = new PIXI.Graphics();
@@ -65,16 +67,23 @@ function createMatchField(levelJSON)
 				graphics.drawRect(0, 0, MATCH_CELL_WIDTH, MATCH_CELL_HEIGHT);
 				graphics.endFill();
 				graphics.cellType = levelJSON.data.Level.cell[index].cellType;
-				graphics.position.x = matchMatrixFrontPosition["i"+i+":j"+j][0];
-				graphics.position.y = matchMatrixFrontPosition["i"+i+":j"+j][1];
-				matchMatrixCell["i"+i+":j"+j] = graphics;
-				matchStage.addChild(matchMatrixCell["i"+i+":j"+j]);
+				graphics.position.x = matchMatrixFrontPosition["i"+iCell+":j"+jCell][0];
+				graphics.position.y = matchMatrixFrontPosition["i"+iCell+":j"+jCell][1];
+				matchMatrixCell["i"+iCell+":j"+jCell] = graphics;
+				matchStage.addChild(matchMatrixCell["i"+iCell+":j"+jCell]);
 			}else{
-				matchMatrixCell["i"+i+":j"+j] = null;
+				matchMatrixCell["i"+iCell+":j"+jCell] = null;
 			}
-			
+			index++;
+		}
+	}
 
-			/* Юнит */
+	/* Юниты */
+	index = 0;
+	for(var iUnit = 0; iUnit < MATCH_COLUMNS; iUnit++)
+	{
+		for(var jUnit = 0; jUnit < MATCH_ROWS; jUnit++)
+		{
 			if(levelJSON.data.Level.cell[index].cellObject != MATCH_HIT_0)
 			{
 				var sprite;
@@ -83,46 +92,49 @@ function createMatchField(levelJSON)
 				if(levelJSON.data.Level.cell[index].cellObject == MATCH_HIT_3) sprite = new PIXI.Sprite(hit3Texture);
 				if(levelJSON.data.Level.cell[index].cellObject == MATCH_HIT_4) sprite = new PIXI.Sprite(hit4Texture);
 				if(levelJSON.data.Level.cell[index].cellObject == MATCH_HIT_5) sprite = new PIXI.Sprite(hit5Texture);
-				sprite.name = "Unit_I_" + i + "_J_" + j;
-				sprite.position.x = matchMatrixFrontPosition["i"+i+":j"+j][0];
-				sprite.position.y = matchMatrixFrontPosition["i"+i+":j"+j][1];
+				sprite.name = "i"+iUnit+":j"+jUnit;
+				sprite.position.x = matchMatrixFrontPosition["i"+iUnit+":j"+jUnit][0];
+				sprite.position.y = matchMatrixFrontPosition["i"+iUnit+":j"+jUnit][1];
 				sprite.interactive = true;
 
 				sprite.unitType = levelJSON.data.Level.cell[index].cellObject;
 				sprite.flagRemove = false;
-				sprite.posColumnI = i;
-				sprite.posRowJ = j;
+				sprite.posColumnI = iUnit;
+				sprite.posRowJ = jUnit;
 
 				sprite.click = onMatchUnitClick;
 				sprite.tap = onMatchUnitClick;
 								
-				matchMatrixUnit["i"+i+":j"+j] = sprite;
-				matchStage.addChild(matchMatrixUnit["i"+i+":j"+j]);
-				console.log("MATCH [Unit]: " + matchMatrixUnit["i"+i+":j"+j].name);
+				matchMatrixUnit["i"+iUnit+":j"+jUnit] = sprite;
+				matchStage.addChild(matchMatrixUnit["i"+iUnit+":j"+jUnit]);
+				console.log("MATCH [Unit]: " + matchMatrixUnit["i"+iUnit+":j"+jUnit].name);
 			}else{
-				matchMatrixUnit["i"+i+":j"+j] = null;
+				matchMatrixUnit["i"+iUnit+":j"+jUnit] = null;
 			}
-
 			index++;
 		}
 	}
+
+
 }
 
 /* Событие: нажатие на юнит */
 function onMatchUnitClick() 
 {
-	matchCellColorSelect(this.unitType, this.posColumnI, this.posRowJ);
-	if(matchSelectUnit1 == null) 
+	if(matchFieldBlocked == false)
 	{
-		matchSelectUnit1 = this;
-	}else{
-		if(matchSelectUnit2 == null) 
+		matchCellColorSelect(this.unitType, this.posColumnI, this.posRowJ);
+		if(matchSelectUnit1 == null) 
 		{
-			matchSelectUnit2 = this;
-			ExchangeUnits(); // меняем юниты местами
+			matchSelectUnit1 = this;
+		}else{
+			if(matchSelectUnit2 == null) 
+			{
+				matchSelectUnit2 = this;
+				matchExchangeUnits(); // меняем юниты местами
+			}
 		}
 	}
-
 	console.log("MATCH [Unit Click]: " + this.unitType);
 }
 
@@ -130,8 +142,12 @@ function onMatchUnitClick()
 function matchCellColorSelect(unitType, colI, rowJ)
 {
 	matchMatrixCell["i"+colI+":j"+rowJ].clear();
-	matchMatrixCell["i"+colI+":j"+rowJ].lineStyle(2, 0xFF00FF, 1);
-	matchMatrixCell["i"+colI+":j"+rowJ].beginFill(0x447700, 0.75);
+	matchMatrixCell["i"+colI+":j"+rowJ].lineStyle(2, 0x000000, 1);
+	if(unitType == MATCH_HIT_1) matchMatrixCell["i"+colI+":j"+rowJ].beginFill(0xFFFF80, 0.50);
+	if(unitType == MATCH_HIT_2) matchMatrixCell["i"+colI+":j"+rowJ].beginFill(0xFF0000, 0.50);
+	if(unitType == MATCH_HIT_3) matchMatrixCell["i"+colI+":j"+rowJ].beginFill(0xFF00FF, 0.50);
+	if(unitType == MATCH_HIT_4) matchMatrixCell["i"+colI+":j"+rowJ].beginFill(0x0080FF, 0.50);
+	if(unitType == MATCH_HIT_5) matchMatrixCell["i"+colI+":j"+rowJ].beginFill(0x00FF80, 0.50);
 	matchMatrixCell["i"+colI+":j"+rowJ].drawRect(0, 0, MATCH_CELL_WIDTH, MATCH_CELL_HEIGHT);
 	matchMatrixCell["i"+colI+":j"+rowJ].endFill();
 }
@@ -151,28 +167,78 @@ function matchCellColorBack()
 }
 
 /* Обмен местами в массиве выбранных пользователем  объектов =================================== */
-function ExchangeUnits()
+function matchExchangeUnits()
+{
+	matchFieldBlocked = true; // поле заблокированно
+
+	var iUnit1 = matchSelectUnit1.posColumnI;
+	var jUnit1 = matchSelectUnit1.posRowJ;
+	var iUnit2 = matchSelectUnit2.posColumnI;
+	var jUnit2 = matchSelectUnit2.posRowJ;
+
+	if(iUnit2 > (iUnit1 - 2) && iUnit2 < (iUnit1 + 2) && jUnit2 > (jUnit1 - 2) && jUnit2 < (jUnit1 + 2) && ((iUnit2 == iUnit1 && jUnit2 != jUnit1) || (jUnit2 == jUnit1 && iUnit2 != iUnit1)))
+	{
+		console.log("MATCH [Unit 1]: I=" + iUnit1 + "   J=" + jUnit1 + "   TYPE="  + matchMatrixUnit["i"+iUnit1+":j"+jUnit1].unitType);
+		matchMatrixUnit["i"+iUnit1+":j"+jUnit1] = matchSelectUnit2;
+		matchMatrixUnit["i"+iUnit1+":j"+jUnit1].posColumnI = iUnit1;
+		matchMatrixUnit["i"+iUnit1+":j"+jUnit1].posRowJ = jUnit1;
+		matchMatrixUnit["i"+iUnit1+":j"+jUnit1].name = "i"+iUnit1+":j"+jUnit1;
+		console.log("MATCH [Unit 1]: I=" + iUnit1 + "   J=" + jUnit1 + "   TYPE="  + matchMatrixUnit["i"+iUnit1+":j"+jUnit1].unitType);
+	
+		console.log("MATCH [Unit 2]: I=" + iUnit2 + "   J=" + jUnit2 + "   TYPE="  + matchMatrixUnit["i"+iUnit2+":j"+jUnit2].unitType);
+		matchMatrixUnit["i"+iUnit2+":j"+jUnit2] = matchSelectUnit1;
+		matchMatrixUnit["i"+iUnit2+":j"+jUnit2].posColumnI = iUnit2;
+		matchMatrixUnit["i"+iUnit2+":j"+jUnit2].posRowJ = jUnit2;
+		matchMatrixUnit["i"+iUnit2+":j"+jUnit2].name = "i"+iUnit2+":j"+jUnit2;
+		console.log("MATCH [Unit 2]: I=" + iUnit2 + "   J=" + jUnit2 + "   TYPE="  + matchMatrixUnit["i"+iUnit2+":j"+jUnit2].unitType);
+	
+		createjs.Tween.get(matchMatrixUnit["i"+iUnit1+":j"+jUnit1], {loop: false})
+			.to({x: matchMatrixFrontPosition["i"+iUnit1+":j"+jUnit1][0], y: matchMatrixFrontPosition["i"+iUnit1+":j"+jUnit1][1]}, 500, createjs.Ease.getPowInOut(4));
+		createjs.Tween.get(matchMatrixUnit["i"+iUnit2+":j"+jUnit2], {loop: false})
+			.to({x: matchMatrixFrontPosition["i"+iUnit2+":j"+jUnit2][0], y: matchMatrixFrontPosition["i"+iUnit2+":j"+jUnit2][1]}, 500, createjs.Ease.getPowInOut(4))
+			.call(onCompleteMatchExchangeUnits);
+		createjs.Ticker.setFPS(60);	
+
+	}else{
+		matchCellColorBack();
+		matchSelectUnitsClear();
+	}
+}
+
+function onCompleteMatchExchangeUnits()
+{
+	matchCellColorBack();
+	matchBackExchangeUnits();
+}
+
+function matchBackExchangeUnits()
 {
 	var iUnit1 = matchSelectUnit1.posColumnI;
 	var jUnit1 = matchSelectUnit1.posRowJ;
 	var iUnit2 = matchSelectUnit2.posColumnI;
 	var jUnit2 = matchSelectUnit2.posRowJ;
 
-
-	console.log("MATCH [Unit 1]: I=" + iUnit1 + "   J=" + jUnit1 + "   TYPE="  + matchMatrixUnit["i"+iUnit1+":j"+jUnit1].unitType);
 	matchMatrixUnit["i"+iUnit1+":j"+jUnit1] = matchSelectUnit2;
-	console.log("MATCH [Unit 1]: I=" + iUnit1 + "   J=" + jUnit1 + "   TYPE="  + matchMatrixUnit["i"+iUnit1+":j"+jUnit1].unitType);
+	matchMatrixUnit["i"+iUnit1+":j"+jUnit1].posColumnI = iUnit1;
+	matchMatrixUnit["i"+iUnit1+":j"+jUnit1].posRowJ = jUnit1;
+	matchMatrixUnit["i"+iUnit1+":j"+jUnit1].name = "i"+iUnit1+":j"+jUnit1;
 	
-	console.log("MATCH [Unit 2]: I=" + iUnit2 + "   J=" + jUnit2 + "   TYPE="  + matchMatrixUnit["i"+iUnit2+":j"+jUnit2].unitType);
 	matchMatrixUnit["i"+iUnit2+":j"+jUnit2] = matchSelectUnit1;
-	console.log("MATCH [Unit 2]: I=" + iUnit2 + "   J=" + jUnit2 + "   TYPE="  + matchMatrixUnit["i"+iUnit2+":j"+jUnit2].unitType);
+	matchMatrixUnit["i"+iUnit2+":j"+jUnit2].posColumnI = iUnit2;
+	matchMatrixUnit["i"+iUnit2+":j"+jUnit2].posRowJ = jUnit2;
+	matchMatrixUnit["i"+iUnit2+":j"+jUnit2].name = "i"+iUnit2+":j"+jUnit2;
 	
 	createjs.Tween.get(matchMatrixUnit["i"+iUnit1+":j"+jUnit1], {loop: false})
-		.to({x: matchMatrixFrontPosition["i"+iUnit1+":j"+jUnit1][0], y: matchMatrixFrontPosition["i"+iUnit1+":j"+jUnit1][1]}, 1000, createjs.Ease.getPowInOut(4));
+		.to({x: matchMatrixFrontPosition["i"+iUnit1+":j"+jUnit1][0], y: matchMatrixFrontPosition["i"+iUnit1+":j"+jUnit1][1]}, 500, createjs.Ease.getPowInOut(4));
 	createjs.Tween.get(matchMatrixUnit["i"+iUnit2+":j"+jUnit2], {loop: false})
-		.to({x: matchMatrixFrontPosition["i"+iUnit2+":j"+jUnit2][0], y: matchMatrixFrontPosition["i"+iUnit2+":j"+jUnit2][1]}, 1000, createjs.Ease.getPowInOut(4));
+		.to({x: matchMatrixFrontPosition["i"+iUnit2+":j"+jUnit2][0], y: matchMatrixFrontPosition["i"+iUnit2+":j"+jUnit2][1]}, 500, createjs.Ease.getPowInOut(4))
+		.call(matchSelectUnitsClear);
 	createjs.Ticker.setFPS(60);	
-
-	matchCellColorBack();
 }
 
+function matchSelectUnitsClear()
+{
+	matchSelectUnit1 = null;
+	matchSelectUnit2 = null;
+	matchFieldBlocked = false; // поле разблокированно
+}
